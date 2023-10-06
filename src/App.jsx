e683f438f79a40b6cef8d73886025af052eaf6f8
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2023-10-06 15:24:34 +0700

    [CREATE]

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ChumChumIcon from "../src/assets/ChumChumLogo-1.png";
+import liff from "@line/liff";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
-  const lineInit = () => {};
+  useEffect(() => {
+    liftInit();
+  }, []);
 
+  const liftInit = async () => {
+    await liff
+      .init({
+        liffId: "2001035033-w8g1yvBj",
+      })
+      .then(async (res) => {
+        const userData = await liff.getProfile();
+        setName(userData.displayName)
+        setImage(userData.pictureUrl)
+        console.log(res);
+        // setMessage("LIFF init succeeded.");
+      })
+      .catch((e) => {
+        // setMessage("LIFF init failed.");
+        // setError(`${e}`);
+      });
+  };
   return (
     <>
       <div className="w-full">
         <div className="sm:container mx-auto p-4">
           <div className="flex justify-center items-center bg-purple-600 p-2 rounded-lg gap-1">
-            <div className="h-10 w-10 bg-green-300">
+            <div className="flex justify-center rounded-md h-10 w-10 bg-green-300">
               <img src={ChumChumIcon} />
             </div>
             <p className="text-white ">Chum Chum Reward</p>
           </div>
 
-          <form className="space-y-2" onSubmit={""}>
+          <form className="space-y-2">
             <div>
               <p className="font-bold text-lg text-center mt-2">ลงทะเบียน</p>
             </div>
             <div className=" ">
-              <label htmlFor="Username">ชื่อผู้ใช้ :</label>
+              <label htmlFor="telephone">เบอร์โทรศัพท์ :</label>
               <br />
               <input
-                id="Username"
-                type="text"
+                id="telephone"
+                type="tel"
                 className="border rounded-md h-10 w-full  p-2"
               />
             </div>
@@ -59,6 +83,8 @@ function App() {
             </div>
           </form>
         </div>
+        <img src={image} className="rounded-full h-24"></img>
+        {name}
       </div>
     </>
   );
