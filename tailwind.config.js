39054778b1d0ce8b0774a31cc6c1ca9665c81bbb
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2023-10-11 19:17:49 +0700

    [UPDATE] modifly reward details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        prompt: ['Prompt','sans-serif'],
+        prompt: ["Prompt", "sans-serif"],
         Rampart: ["Rampart One", "sans-serif"],
-        'Pixelify-Sans':['Pixelify Sans',"sans-serif"]
+        "Pixelify-Sans": ["Pixelify Sans", "sans-serif"],
+      },
+      height: {
+        13: "3.25rem",
+        18: "4.5rem",
+        54: "13.5rem",
+        76: "19rem",
+        84: "21rem",
+        88: "22rem",
+        92: "23rem",
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        176: "44rem",
+        192: "52rem",
+      },
+      width: {
+        "70%": "70%",
+        "30%": "30%",
+        13: "3.25rem",
+        18: "4.5rem",
+        54: "13.5rem",
+        76: "19rem",
+        84: "21rem",
+        88: "22rem",
+        92: "23rem",
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        160: "40rem",
+        176: "44rem",
+        192: "52rem",
       },
     },
   },
