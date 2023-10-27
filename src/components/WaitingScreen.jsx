d6da1aaeee46312590edeaff29c45110d0eb3ba9
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2023-10-27 18:09:01 +0700

    [UPDATE] alert

--- src/components/WaitingScreen.jsx
+++ src/components/WaitingScreen.jsx
@@ -7,7 +7,6 @@ const WaitingScreen = ({isOpen}) => {
       <div className="modal-overlay fixed inset-0 bg-black opacity-30"></div>
       <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
         <div className="modal-content py-4 text-left px-6">
-        dsdasd
         </div>
       </div>
     </div>
